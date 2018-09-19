8b548174e538dea246265bb44ed100f0fa448d3f
NickRocchi <42780504+NickRocchi@users.noreply.github.com>
2018-09-19 11:18:29 -0700

    yeeticus

---
+++ Emmm.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class Emmm here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Emmm  
+{
+    // instance variables - replace the example below with your own
+    public void sleepy();
+    public void stressed();
+    public void roast();
+    public void mrKams();  
+    public void moveOut();
+}
